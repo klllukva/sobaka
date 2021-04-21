1caa1d446070994bb51ec0c05869dc56ad6e5702
Гость <@b302-05.sttec.net>
2021-04-21 10:11:19 +0300

    i_am_Doshick

---
+++ 1zadanie/2zadanie/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2zadanie
+{
+    public partial class Form1 : Form
+    {
+        int[] Mas = new int[21];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Random rand = new Random();
+            for (int i = 0; i < 21; i++)
+            {
+                Mas[i] = rand.Next(0, 20);
+                listBox1.Items.Add("Mas [" + i.ToString() + "] =" + Mas[i].ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            int Max = Mas[0], temp = 0, count = 0;
+            for (int i = 0; i < 21; i++)
+            {
+                if (Mas[i] > Max)
+                {
+                    Max = Mas[i];
+                    count = i;
+                }
+
+            }
+            temp = Mas[0];
+            Mas[0] = Max;
+            Mas[count] = temp;
+            for (int i = 0; i < 21; i++)
+            {
+                listBox2.Items.Add("Mas [" + Convert.ToString(i) + "] =" + Mas[i].ToString());
+            }
+        }
+    }
+}
---
+++ 2zadanie/2zadanie/2zadanie/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2zadanie
+{
+    public partial class Form1 : Form
+    {
+        int[] Mas = new int[11];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Random rand = new Random();
+            for (int i = 0; i < 11; i++)
+            {
+                Mas[i] = rand.Next(0, 10);
+                listBox1.Items.Add("Mas [" + i.ToString() + "] =" + Mas[i].ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            listBox2.Items.Clear();
+            int Min = Mas[0], temp = 0, count = 0;
+            for (int i = 0; i < 11; i++)
+            {
+                if (Mas[i] > Min)
+                {
+                    Min = Mas[i];
+                    count = i;
+                }
+
+            }
+            temp = Mas[0];
+            Mas[0] = Min;
+            Mas[count] = temp;
+            for (int i = 0; i < 11; i++)
+            {
+                listBox2.Items.Add("Mas [" + Convert.ToString(i) + "] =" + Mas[i].ToString());
+            }
+        }
+    }
+}
